e92e96ff713092647495bc49704c651fbf9ba738
Polina Ruleva <daygreyki@gmail.com>
2026-03-16 20:53:18 +0400

    vite config fixed

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names?.some(name => /\.(otf|ttf|woff2?|eot)$/i.test(name))) {
+            return 'fonts/[name][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        },
+      },
+    },
+  },
 })
